c06af7ff12c0285244456bf68dda2e3e6dc2354c
Hod Bin Noon <hodbn@users.noreply.github.com>
2023-05-22 17:52:43 +0300

    feat: add template of scoring functions

---
+++ ex2/ex2/score.py
@@ -0,0 +1,31 @@
+def freq_score(text: str) -> float:
+    """
+    go over the text
+    remove punctuation including spaces
+    create a frequency table for english letter
+    read real english frequency table from Letter_Freq.txt
+    compare the tables and create a score (???)
+    """
+    pass
+
+
+def freq2_score(text: str) -> float:
+    """
+    go over the text
+    remove punctuation
+    create a frequency table for pairs of english letters
+    drop all pairs with spaces
+    read real english frequency table from Letter2_Freq.txt
+    compare the tables and create a score (???)
+    """
+    pass
+
+
+def dict_score(text: str) -> float:
+    """
+    go over text
+    remove punctuation
+    split to words
+    return the ratio of words that exist in dict.txt
+    """
+    pass
