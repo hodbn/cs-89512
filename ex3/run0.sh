d79f91a26e5ed2c2820e044eba58ab0a757057dc
Hod Bin Noon <hodbn@users.noreply.github.com>
2023-06-25 20:44:52 +0300

    chore(ex3): refactor run scripts

--- ex3/run0.sh
+++ ex3/run0.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 buildnet0.py && python3 gentestnet0.py && python3 runnet0.py && python3 measurenet0.py
+python3 buildnet0.py && python3 gentestnet.py 0 && python3 runnet0.py && python3 measurenet.py 0
 rm testnet0 testnet0_real testnet0_predicted
