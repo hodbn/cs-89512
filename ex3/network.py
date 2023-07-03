17b8ad13995e68aa98e16ff405d74e837263649f
Hod Bin Noon <hodbn@users.noreply.github.com>
2023-07-03 22:28:18 +0300

    refactor(ex3): remove some comments

--- ex3/network.py
+++ ex3/network.py
@@ -1,16 +1,14 @@
+from typing import Protocol
+
 import numpy as np
 
 
-class Layer:
-    def __init__(self):
-        self.inp = None
-        self.out = None
-
+class Layer(Protocol):
     def forward_propagation(self, inp):
-        raise NotImplementedError()
+        ...
 
     def backward_propagation(self, out_error, learn_rate):
-        raise NotImplementedError()
+        ...
 
 
 class FCLayer(Layer):
@@ -42,14 +40,11 @@ class ActivationLayer(Layer):
         self.activation = activation
         self.activation_prime = activation_prime
 
-    # returns the activated input
     def forward_propagation(self, inp):
         self.inp = inp
         self.out = self.activation(self.inp)
         return self.out
 
-    # Returns input_error=dE/dX for a given output_error=dE/dY.
-    # learning_rate is not used because there is no "learnable" parameters.
     def backward_propagation(self, out_error, learn_rate):
         return self.activation_prime(self.inp) * out_error
 
@@ -63,24 +58,18 @@ class Network:
         self.loss = None
         self.loss_prime = None
 
-    # add layer to network
     def add(self, layer):
         self.layers.append(layer)
 
-    # set loss to use
     def use(self, loss, loss_prime):
         self.loss = loss
         self.loss_prime = loss_prime
 
-    # predict output for given input
     def predict(self, input_data):
-        # sample dimension first
         samples = len(input_data)
         result = []
 
-        # run network over all samples
         for i in range(samples):
-            # forward propagation
             output = input_data[i]
             for layer in self.layers:
                 output = layer.forward_propagation(output)
@@ -88,29 +77,22 @@ class Network:
 
         return result
 
-    # train the network
     def fit(self, x_train, y_train, epochs, learning_rate):
-        # sample dimension first
         samples = len(x_train)
 
-        # training loop
         for i in range(epochs):
             err = 0
             for j in range(samples):
-                # forward propagation
                 output = x_train[j]
                 for layer in self.layers:
                     output = layer.forward_propagation(output)
 
-                # compute loss (for display purpose only)
                 err += self.loss(y_train[j], output)
 
-                # backward propagation
                 error = self.loss_prime(y_train[j], output)
                 for layer in reversed(self.layers):
                     error = layer.backward_propagation(error, learning_rate)
 
-            # calculate average error on all samples
             err /= samples
             print("epoch %d/%d   error=%f" % (i + 1, epochs, err))
 
